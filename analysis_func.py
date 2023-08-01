a8fd6e303c88ece4dbdfa47127fcbc011a25ae3f
yi-chia-chen <yichiachen@g.ucla.edu>
2023-08-01 11:48:54 -0700

    reformat

--- analysis_func.py
+++ analysis_func.py
@@ -7,129 +7,154 @@ from scipy import stats
 from matplotlib import pyplot as plt
 import seaborn as sns
 
- ######   ######## ##    ## ######## ########     ###    ##
-##    ##  ##       ###   ## ##       ##     ##   ## ##   ##
-##        ##       ####  ## ##       ##     ##  ##   ##  ##
-##   #### ######   ## ## ## ######   ########  ##     ## ##
-##    ##  ##       ##  #### ##       ##   ##   ######### ##
-##    ##  ##       ##   ### ##       ##    ##  ##     ## ##
- ######   ######## ##    ## ######## ##     ## ##     ## ########
-
-def CHECK_AND_CREATE_FOLDERS(path):
-    PATH = path.strip('/').strip('\\')
-    if not os.path.exists(PATH):
-        os.makedirs(PATH)
-
-def TODAY_DATE_STRING():
-    return str(datetime.now()).split('.')[0].split(' ')[0].replace('-', '.')
+#  ######   ######## ##    ## ######## ########     ###    ##
+# ##    ##  ##       ###   ## ##       ##     ##   ## ##   ##
+# ##        ##       ####  ## ##       ##     ##  ##   ##  ##
+# ##   #### ######   ## ## ## ######   ########  ##     ## ##
+# ##    ##  ##       ##  #### ##       ##   ##   ######### ##
+# ##    ##  ##       ##   ### ##       ##    ##  ##     ## ##
+#  ######   ######## ##    ## ######## ##     ## ##     ## ########
 
 
- ######  ########    ###    ######## ####  ######  ######## ####  ######   ######
-##    ##    ##      ## ##      ##     ##  ##    ##    ##     ##  ##    ## ##    ##
-##          ##     ##   ##     ##     ##  ##          ##     ##  ##       ##
- ######     ##    ##     ##    ##     ##   ######     ##     ##  ##        ######
-      ##    ##    #########    ##     ##        ##    ##     ##  ##             ##
-##    ##    ##    ##     ##    ##     ##  ##    ##    ##     ##  ##    ## ##    ##
- ######     ##    ##     ##    ##    ####  ######     ##    ####  ######   ######
+def check_and_create_folders(path):
+    path = path.strip("/").strip("\\")
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-def CALCULATE_SS_FROM_MEAN(number_list):
+
+def get_date_string():
+    return str(datetime.now()).split(".")[0].split(" ")[0].replace("-", ".")
+
+
+#  ######  ########    ###    ######## ####  ######  ######## ####  ######   ######
+# ##    ##    ##      ## ##      ##     ##  ##    ##    ##     ##  ##    ## ##    ##
+# ##          ##     ##   ##     ##     ##  ##          ##     ##  ##       ##
+#  ######     ##    ##     ##    ##     ##   ######     ##     ##  ##        ######
+#       ##    ##    #########    ##     ##        ##    ##     ##  ##             ##
+# ##    ##    ##    ##     ##    ##     ##  ##    ##    ##     ##  ##    ## ##    ##
+#  ######     ##    ##     ##    ##    ####  ######     ##    ####  ######   ######
+
+
+def calculate_ss_from_mean(number_list):
     count = len(number_list)
-    mean = float(sum(number_list))/count
-    return functools.reduce(lambda x, y: x+ (y-mean)**2, number_list, 0)
+    mean = float(sum(number_list)) / count
+    return functools.reduce(lambda x, y: x + (y - mean) ** 2, number_list, 0)
 
-def CALCULATE_SD(number_list):
+
+def calculate_sd(number_list):
     count = len(number_list)
-    return CALCULATE_SS_FROM_MEAN(number_list) / (count-1)
+    return CALCULATE_SS_FROM_MEAN(number_list) / (count - 1)
 
-def CALCULATE_SIGMA(number_list):
+
+def calculate_sigma(number_list):
     count = len(number_list)
     return CALCULATE_SS_FROM_MEAN(number_list) / count
 
-def SPLIT_HALF_RELIABILITY_CORRECTION(r):
-    return 2*r / (1+abs(r))
 
-def EUCLIDEAN_DISTANCE(vector_1, vector_2):
-    assert len(vector_1) == len(vector_2), 'ERROR: vectors have unequal lengths'
-    return sum([(x_1-x_2)**2 for x_1,x_2 in zip(vector_1,vector_2)]) ** 0.5
+def split_half_reliability_correction(r):
+    return 2 * r / (1 + abs(r))
 
-def SUM_SQUARES(number_list):
-    return functools.reduce(lambda x, y: x+y**2, number_list, 0)
 
-def SUM_X_Y_MULTIPLY(x_list, y_list):
-    assert len(x_list) == len(y_list), 'ERROR: input lists have unequal lengths'
-    return functools.reduce(lambda x, y: x + y[0]*y[1], zip(x_list,y_list), 0)
+def euclidean_distance(vector_1, vector_2):
+    assert len(vector_1) == len(vector_2), "ERROR: vectors have unequal lengths"
+    return sum([(x_1 - x_2) ** 2 for x_1, x_2 in zip(vector_1, vector_2)]) ** 0.5
 
-def CALCULATE_PEARSON_R(x_list, y_list):
+
+def sum_squares(number_list):
+    return functools.reduce(lambda x, y: x + y**2, number_list, 0)
+
+
+def sum_x_y_multiply(x_list, y_list):
+    assert len(x_list) == len(y_list), "ERROR: input lists have unequal lengths"
+    return functools.reduce(lambda x, y: x + y[0] * y[1], zip(x_list, y_list), 0)
+
+
+def calculate_pearson_r(x_list, y_list):
     count = len(x_list)
-    assert count == len(y_list), 'ERROR: input lists have unequal lengths'
-    mean_x = sum(x_list)/count
-    mean_y = sum(y_list)/count
-    SS_x = SUM_SQUARES(x_list)
-    SS_y = SUM_SQUARES(y_list)
-    sum_xy = SUM_X_Y_MULTIPLY(x_list,y_list)
-    SD_x = ((SS_x - count*(mean_x**2))) ** 0.5
-    SD_y = ((SS_y - count*(mean_y**2))) ** 0.5
-    r = (sum_xy - mean_x*mean_y*count) / (SD_x*SD_y)
+    assert count == len(y_list), "ERROR: input lists have unequal lengths"
+    mean_x = sum(x_list) / count
+    mean_y = sum(y_list) / count
+    ss_x = SUM_SQUARES(x_list)
+    ss_y = SUM_SQUARES(y_list)
+    sum_xy = SUM_X_Y_MULTIPLY(x_list, y_list)
+    sd_x = ((ss_x - count * (mean_x**2))) ** 0.5
+    sd_y = ((ss_y - count * (mean_y**2))) ** 0.5
+    r = (sum_xy - mean_x * mean_y * count) / (sd_x * sd_y)
     if abs(r) != 1:
-        tt = abs(r)*((count-2)**0.5)/((1-r**2)**0.5)
-        p = stats.t.sf(tt, count-2)*2 # two-tailed p-value
+        tt = abs(r) * ((count - 2) ** 0.5) / ((1 - r**2) ** 0.5)
+        p = stats.t.sf(tt, count - 2) * 2  # two-tailed p-value
     else:
         p = 0
     return (r, p)
 
 
-########     ###    ##    ## ########     ###     ######
-##     ##   ## ##   ###   ## ##     ##   ## ##   ##    ##
-##     ##  ##   ##  ####  ## ##     ##  ##   ##  ##
-########  ##     ## ## ## ## ##     ## ##     ##  ######
-##        ######### ##  #### ##     ## #########       ##
-##        ##     ## ##   ### ##     ## ##     ## ##    ##
-##        ##     ## ##    ## ########  ##     ##  ######
+# ########     ###    ##    ## ########     ###     ######
+# ##     ##   ## ##   ###   ## ##     ##   ## ##   ##    ##
+# ##     ##  ##   ##  ####  ## ##     ##  ##   ##  ##
+# ########  ##     ## ## ## ## ##     ## ##     ##  ######
+# ##        ######### ##  #### ##     ## #########       ##
+# ##        ##     ## ##   ### ##     ## ##     ## ##    ##
+# ##        ##     ## ##    ## ########  ##     ##  ######
 
-def CLEAN_CARRIAGE_RETURN(df):
+
+def clean_carriage_return(df):
     df = df.copy()
     last_column_name = list(df.columns)[-1]
-    if last_column_name[-1] == '\r':
+    if last_column_name[-1] == "\r":
         new_column_name = last_column_name[:-1]
         df.columns = list(df.columns[:-1]) + [new_column_name]
-        df[new_column_name] = df[new_column_name].str.rstrip('\r')
+        df[new_column_name] = df[new_column_name].str.rstrip("\r")
     return df
 
-def DELETE_REPEATING_TITLES(df):
+
+def delete_repeating_titles(df):
     df = df.copy()
     first_column_name = df.columns[0]
     df = df.drop(df.loc[df[first_column_name] == first_column_name].index, axis=0)
     return df
 
 
- ######  ########    ###    ########   #######  ########  ##    ##
-##    ## ##         ## ##   ##     ## ##     ## ##     ## ###   ##
-##       ##        ##   ##  ##     ## ##     ## ##     ## ####  ##
- ######  ######   ##     ## ########  ##     ## ########  ## ## ##
-      ## ##       ######### ##     ## ##     ## ##   ##   ##  ####
-##    ## ##       ##     ## ##     ## ##     ## ##    ##  ##   ###
- ######  ######## ##     ## ########   #######  ##     ## ##    ##
+#  ######  ########    ###    ########   #######  ########  ##    ##
+# ##    ## ##         ## ##   ##     ## ##     ## ##     ## ###   ##
+# ##       ##        ##   ##  ##     ## ##     ## ##     ## ####  ##
+#  ######  ######   ##     ## ########  ##     ## ########  ## ## ##
+#       ## ##       ######### ##     ## ##     ## ##   ##   ##  ####
+# ##    ## ##       ##     ## ##     ## ##     ## ##    ##  ##   ###
+#  ######  ######## ##     ## ########   #######  ##     ## ##    ##
 
-def PLOT_DISTRIBUTION(
-    data_list, color='#3d879c', hist=True, kde=True, rug=True,
-    x_limit=None, y_limit=None,
-    x_label=None, y_label=None,
-    save=False, today_date=None, dpi=200):
 
-    PLOT_SETTINGS_DIC = {
-        'axes.grid': False,
-        'font.size':10,
-        'grid.color': '.5',
-        'font.sans-serif': ['Century Gothic', 'Arial', 'sans-serif'],
-        'axes.spines.left': False,
-        'axes.spines.bottom': True,
-        'axes.spines.right': False,
-        'axes.spines.top': False
-        }
-    with sns.axes_style('darkgrid',rc=PLOT_SETTINGS_DIC):
-        plot = sns.distplot(data_list,
-                        hist=hist, kde=kde, rug=rug,
-                        color=color, kde_kws={'linewidth':4})
+def plot_distribution(
+    data_list,
+    color="#3d879c",
+    hist=True,
+    kde=True,
+    rug=True,
+    x_limit=None,
+    y_limit=None,
+    x_label=None,
+    y_label=None,
+    save=False,
+    dpi=200,
+):
+    plot_settings = {
+        "axes.grid": False,
+        "font.size": 10,
+        "grid.color": ".5",
+        "font.sans-serif": ["Century Gothic", "Arial", "sans-serif"],
+        "axes.spines.left": False,
+        "axes.spines.bottom": True,
+        "axes.spines.right": False,
+        "axes.spines.top": False,
+    }
+    with sns.axes_style("darkgrid", rc=plot_settings):
+        plot = sns.distplot(
+            data_list,
+            hist=hist,
+            kde=kde,
+            rug=rug,
+            color=color,
+            kde_kws={"linewidth": 4},
+        )
         plot.set(yticks=[])
         if x_limit != None:
             plt.xlim(x_limit[0], x_limit[1])
@@ -140,5 +165,10 @@ def PLOT_DISTRIBUTION(
         if y_label != None:
             plt.ylabel(y_label, labelpad=10)
         if save:
-            plt.savefig('distribution_'+today_date+'.png', dpi=dpi, bbox_inches='tight')
-            plt.savefig('distribution_'+today_date+'.svg', dpi=dpi, bbox_inches='tight')
+            today_date = get_date_string()
+            plt.savefig(
+                "distribution_" + today_date + ".png", dpi=dpi, bbox_inches="tight"
+            )
+            plt.savefig(
+                "distribution_" + today_date + ".svg", dpi=dpi, bbox_inches="tight"
+            )
